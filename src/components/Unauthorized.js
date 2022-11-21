6c0427a612b5b7d0af07b254aee01173990a638f
Hoang Anh <89508177+MarcoLuw@users.noreply.github.com>
2022-11-22 01:07:56 +0700

    Delete src/components directory

--- src/components/Unauthorized.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Unauthorized = () => {
-  return (
-    <h4>401 Unauthorized</h4>
-  )
-}
-
-export default Unauthorized
--- src/components/admin/SmallImage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const SmallImage = ({src}) => {
-  return (
-    <img src={src} style={{ width:"100px", height:"100px" }}/>
-  )
-}
-
-export default SmallImage
