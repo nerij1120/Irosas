050d5884ab93e1b157ce011fccfe6ba9c064a816
anh2502 <nguyenkieuanhbl@gmail.com>
2022-11-24 12:48:29 +0700

    new

---
+++ public/myscriptfile/script.js
@@ -0,0 +1,8 @@
+function hiddenFunc() {
+    document.getElementById("menu-button").style.display = "None";
+    document.getElementById("menu-button2").style.display = "block";
+}
+
+function showDialog() {
+    document.getElementById("dialog").style.display = "block";
+}
---
+++ src/pages/main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from 'react-dom/client';
+export default function Main(){
+    
+<header>
+    hello
+</header>
+
+}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(Main);
