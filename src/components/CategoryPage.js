a011f2849139aa2011f82f8dc48b97a60e098568
nhnv13122001 <nhnv13122001@gmail.com>
2022-11-25 14:35:48 +0700

    Hoàn thiện Category

--- src/components/CategoryPage.js
+++ src/components/CategoryPage.js
@@ -1,6 +1,4 @@
 import React from "react";
-import silderdanhmuc from "../img/CategoryPage/sliderdanhmuc.jpg";
-import slider from "../img/CategoryPage/slider.jpg";
 import s1 from "../img/CategoryPage/1.jpg";
 import s2 from "../img/CategoryPage/2.jpg";
 import s3 from "../img/CategoryPage/3.jpg";
@@ -10,69 +8,84 @@ import s6 from "../img/CategoryPage/6.jpg";
 import s7 from "../img/CategoryPage/7.jpg";
 import s8 from "../img/CategoryPage/8.jpg";
 import s9 from "../img/CategoryPage/9.jpg";
-import MyNav from "./MyNav";
-import Footer from "./Footer";
+import { Button, Col, Container, Row } from "react-bootstrap";
+import CategoryItem from "./CategoryItem";
 
 const CategoryPage = () => {
   return (
     <div>
-      <img style={{ width: "100%" }} src={slider} alt="" />
-      <div>
-        <div>Danh mục thức uống</div>
-        <div className="d-flex flex-column align-items-center">
-          <div className="d-flex flex-row ">
-            <img
-              style={{ width: "200px", height: "200px", margin: "10px 50px" }}
-              src={s1}
-              alt=""
-            />
-            <img
-              style={{ width: "200px", height: "200px", margin: "10px 50px" }}
-              src={s2}
-              alt=""
-            />
-            <img
-              style={{ width: "200px", height: "200px", margin: "10px 50px" }}
-              src={s3}
-              alt=""
-            />
-          </div>
-          <div className="d-flex flex-row  ">
-            <img
-              style={{ width: "200px", height: "200px", margin: "10px 50px" }}
-              src={s4}
-              alt=""
-            />
-            <img
-              style={{ width: "200px", height: "200px", margin: "10px 50px" }}
-              src={s5}
-              alt=""
-            />
-            <img
-              style={{ width: "200px", height: "200px", margin: "10px 50px" }}
-              src={s6}
-              alt=""
-            />
-          </div>
-          <div className="d-flex flex-row  ">
-            <img
-              style={{ width: "200px", height: "200px", margin: "10px 50px" }}
-              src={s7}
-              alt=""
-            />
-            <img
-              style={{ width: "200px", height: "200px", margin: "10px 50px" }}
-              src={s8}
-              alt=""
-            />
-            <img
-              style={{ width: "200px", height: "200px", margin: "10px 50px" }}
-              src={s9}
-              alt=""
-            />
-          </div>
-        </div>
-      </div>
+      <Container
+        fluid
+        style={{
+          backgroundImage: `url(/images/dau_trang.png`,
+          height: "80vh",
+          backgroundSize: "100% 100%",
+          display: "flex",
+        }}
+      >
+        <Row className="my-auto ms-5">
+          <p id="p1">Đầy đủ các danh mục</p>
+          <p id="p2">cho bạn lựa chọn</p>
+          <p id="p3">
+            Mỗi một loại thức uống sẽ cho bạn những trải nghiệm mới mẻ khác
+            nhau.
+          </p>
+          <Col>
+            <Button Button className="btn-default get">
+              Xem ngay <i class="fas fa-long-arrow-right" id="arrowGet"></i>
+            </Button>
+          </Col>
+        </Row>
+      </Container>
+      <p
+        className="ms-2"
+        style={{
+          fontFamily: "DM Serif Display",
+          fontWeight: "700",
+          fontSize: "36px",
+          lineHeight: "48px",
+          color: "#D0B8A8",
+        }}
+      >
+        Danh mục thức uống
+      </p>
+      <Container className="mt-4 text-center">
+        <Row className="mt-3 justify-content-md-center">
+          <Col className="col-md-3 col-sm-1">
+            <CategoryItem src={s1} alt="s1" text="Coffee" />
+          </Col>
+          <Col className="col-md-3 col-sm-1">
+            <CategoryItem src={s2} alt="s1" text="Tea" />
+          </Col>
+          <Col className="col-md-3 col-sm-1">
+            <CategoryItem src={s3} alt="s1" text="Ice Blended" />
+          </Col>
+        </Row>
+
+        <Row className="mt-3 justify-content-md-center">
+          <Col className="col-md-3 col-sm-1">
+            <CategoryItem src={s4} alt="s1" text="Chocolate" />
+          </Col>
+          <Col className="col-md-3 col-sm-1">
+            <CategoryItem src={s5} alt="s1" text="Fruit Juice" />
+          </Col>
+          <Col className="col-md-3 col-sm-1">
+            <CategoryItem src={s6} alt="s1" text="Mile Tea" />
+          </Col>
+        </Row>
+
+        <Row className="mt-3 justify-content-md-center">
+          <Col className="col-md-3 col-sm-1">
+            <CategoryItem src={s7} alt="s1" text="Cocktail" />
+          </Col>
+          <Col className="col-md-3 col-sm-1">
+            <CategoryItem src={s8} alt="s1" text="Shakes" />
+          </Col>
+          <Col className="col-md-3 col-sm-1">
+            <CategoryItem src={s9} alt="s1" text="Soft Drink" />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
